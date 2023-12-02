e657bfadbecadd7f398e92bdd23f1b7de9967fa1
Adam Carless <adam.carless@ba.com>
2023-12-02 13:39:56 +0000

    day 2

---
+++ day2/aoc.java
@@ -0,0 +1,74 @@
+// https://adventofcode.com/2022/day/1
+import java.nio.file.*;
+import java.util.*;
+import java.util.regex.*;
+
+record CubeSet(int red, int green, int blue) {
+  boolean greaterThan(CubeSet b) { 
+    return red > b.red() || green > b.green() || blue > b.blue(); 
+  }
+  int power() { return red * green * blue; }
+}
+
+record Game(int id, CubeSet max){}
+
+ArrayList<Game> games = new ArrayList<>();
+
+void main(String[] args) throws Exception {
+  slurp(args[0]);
+  System.out.println("part 1: " + part1());
+  System.out.println("part 2: " + part2());
+}
+
+// load text file and parse into games
+void slurp(String filename) throws Exception {
+  Files.lines(Path.of(filename)).forEach(l -> { parseGame(l); });
+}
+
+int part1() {
+  CubeSet threshold = new CubeSet(12, 13, 14);
+  return games.stream().mapToInt((g) -> g.max().greaterThan(threshold) ? 0 : g.id()).sum();
+}
+
+int part2() {
+  return games.stream().mapToInt((g) -> g.max().power()).sum();
+}
+
+// turn input string into a game with known colour maxes
+void parseGame(String game) {
+  games.add(new Game(
+    gameId(game),
+    maxes(game)
+  ));
+}
+
+// find the game id (don't trust them to be a sequence!)
+Pattern idRe = Pattern.compile("^Game (\\d+)\\:");
+int gameId(String game) {
+  Matcher m = idRe.matcher(game);
+  m.find();
+  return Integer.parseInt(m.group(1));
+}
+
+// find the maxes for each colour in a game
+Pattern redRe = Pattern.compile("(\\d+) red");
+Pattern greenRe = Pattern.compile("(\\d+) green");
+Pattern blueRe = Pattern.compile("(\\d+) blue");
+CubeSet maxes(String game) {
+  return new CubeSet(
+    getMaxMatch(redRe.matcher(game)),
+    getMaxMatch(greenRe.matcher(game)),
+    getMaxMatch(blueRe.matcher(game))
+  );
+}
+
+// Java's regexp handling _really_ needs improving
+// especially now streams and lambdas exist.
+int getMaxMatch(Matcher m) {
+  int max = 0;
+  while (m.find()) {
+    int i = Integer.parseInt(m.group(1));
+    max = i > max ? i : max;
+  }
+  return max;
+}
